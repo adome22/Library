188c31d424f71a80719ec3c0af1f79ec7dbfc446
Elmeri <62933497+adome22@users.noreply.github.com>
2024-11-11 13:40:09 +0200

    Modal opens/closesnow

--- app.js
+++ app.js
@@ -24,13 +24,34 @@ const addBookToLibrary = () => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const isRead = document.getElementById('isRead').checked;
+    const read = document.getElementById('isRead').checked;
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
+// UI
+
+const bookModal = document.getElementById("form-modal");
+const addBookBtn = document.getElementById("add-book-btn");
+const closeModalBtn = document.getElementById("close-btn");
+const addBookForm = document.getElementById("add-book-form");
+const bookGrid = document.getElementById("book-grid");
+const overlay = document.querySelector(".overlay");
 
 
+const openAddBookModal = () => {
+    bookModal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+const closeBookModal = () => {
+    bookModal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+
+addBookBtn.onclick = openAddBookModal
+closeModalBtn.onclick = closeBookModal
 
 
 
