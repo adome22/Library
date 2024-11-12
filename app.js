e8068b5b1fcc78108151ade1b134ea1b0b007eaf
Elmeri <62933497+adome22@users.noreply.github.com>
2024-11-12 13:25:33 +0200

    Added more code

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-let myLibrary = [
-    {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        pages: 304,
-        read: false
-    },
-    {
-        title: "The Lord of the Rings",
-        author: "J.R.R Tolkien",
-        pages: 1178,
-        read: false
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = '0',
+        isRead = false
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
     }
-];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
 }
 
-const addBookToLibrary = () => {
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const read = document.getElementById('isRead').checked;
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(newBook) {
+        if (!this.isInLibrary(newBook)) {
+            this.books.push(newBook);
+        }
+    }
+
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title);
+    }
+    getBook(title) {
+        return this.books.find((book) => book.title === title);
+    }
+
+    isInLibrary(newBook) {
+        return this.books.some((book) => book.title === newBook.title);
+    }
 }
+
+const library = new Library();
+
 
 // UI
 
 const bookModal = document.getElementById("form-modal");
-const addBookBtn = document.getElementById("add-book-btn");
+const addBookBtn = document.getElementById("btn-add");
 const closeModalBtn = document.getElementById("close-btn");
-const addBookForm = document.getElementById("add-book-form");
-const bookGrid = document.getElementById("book-grid");
+const addBookForm = document.getElementById("addBookForm");
+const bookGrid = document.getElementById("booksGrid");
 const overlay = document.querySelector(".overlay");
 
 
@@ -49,15 +58,120 @@ const closeBookModal = () => {
     overlay.classList.remove('active');
 }
 
+const handleKeyboardInput = (e) => {
+    if (e.key === 'Escape') closeBookModal();
+}
+
+const updateBooksGrid = () => {
+    resetBooksGrid();
+    for (let book of library.books) {
+        createBookCard(book);
+    }
+}
+
+const resetBooksGrid = () => {
+    bookGrid.innerHTML = '';
+}
+
 const createBookCard = (book) => {
     const bookCard = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const buttons = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    bookCard.classList.add('book-card');
+    buttons.classList.add('buttons');
+    readBtn.classList.add('btn');
+    removeBtn.classList.add('btn');
+    readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = 'Remove';
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    buttons.appendChild(readBtn);
+    buttons.appendChild(removeBtn);
+    bookCard.appendChild(buttons);
+    bookGrid.appendChild(bookCard);
 }
 
-addBookBtn.onclick = openAddBookModal
-closeModalBtn.onclick = closeBookModal
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('isRead').checked;
+    return new Book(title, author, pages, isRead);
+}
+
+const addBook = (e) => {
+    e.preventDefault();
+    const newBook = getBookFromInput();
+
+    if (library.isInLibrary(newBook)) {
+        errorMsg.textContent = 'Book already exists'
+        errorMsg.classList.add('active');
+        return
+    }
+
+    library.addBook(newBook);
+    saveLocal();
+    updateBooksGrid()
+    closeBookModal();
+}
+
+const removeBook = (e) => {
+    const title = e.target.parentNode.parentNode.
+    firstChild.innerHTML.replaceAll('"', '');
+
+    library.removeBook(title);
+    saveLocal();
+    updateBooksGrid();
+}
+
+const toggleRead = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    );
+    const book = library.getBook(title);
+    
+    book.isRead = !book.isRead;
+    saveLocal();
+    updateBooksGrid();
+}
 
 
+addBookBtn.onclick = openAddBookModal;
+closeModalBtn.onclick = closeBookModal;
+overlay.onclick = closeBookModal;
+addBookForm.onsubmit = addBook;
+window.onkeydown = handleKeyboardInput;
 
+// Local Storage
 
+const saveLocal = () => {
+    localStorage.setItem('library', JSON.stringify(library.books))
+}
+
+const restoreLocal = () => {
+    const books = JSON.parse(localStorage.getItem('library'));
+    if (books) {
+        library.books = books.map((book) => JSONToBook(book));
+    } else {
+        library.books = [];
+    }
+}
+
+// Utils
+
+const JSONToBook = (book) => {
+    return newBook(book.title, book.author, book.pages, book.isRead);
+}
